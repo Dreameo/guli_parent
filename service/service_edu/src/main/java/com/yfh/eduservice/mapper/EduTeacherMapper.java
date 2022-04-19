321c18bf64610f4f080b4a0845a7c38c68166240
lab507_win <yfh97@qq.com>
2022-04-19 16:54:03 +0800

    谷粒商城环境搭建---test代码生成器

---
+++ service/service_edu/src/main/java/com/yfh/eduservice/mapper/EduTeacherMapper.java
@@ -0,0 +1,16 @@
+package com.yfh.eduservice.mapper;
+
+import com.yfh.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author testjava
+ * @since 2022-04-19
+ */
+public interface EduTeacherMapper extends BaseMapper<EduTeacher> {
+
+}
---
+++ service/service_edu/src/main/java/com/yfh/eduservice/service/impl/EduTeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package com.yfh.eduservice.service.impl;
+
+import com.yfh.eduservice.entity.EduTeacher;
+import com.yfh.eduservice.mapper.EduTeacherMapper;
+import com.yfh.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2022-04-19
+ */
+@Service
+public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacher> implements EduTeacherService {
+
+}
